bb3901bf850dafd224fcdf344a5357a7dc2d5b17
Viktor Filippov <jackal3166@gmail.com>
2023-09-24 23:14:45 +0300

    Create Tour

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,4 +1,14 @@
-const Tour = () => {
-    return <h2>Tour</h2>;
+const Tour = ({ id, image, info, price, name }) => {
+    return (
+        <article className='single-tour'>
+            <img src={image} alt='name' className='img' />
+            <span className='tour-price'>${price}</span>
+            <div className="tour-info">
+                <h5>{name}</h5>
+                <p>{info}</p>
+            </div>
+        </article>
+    );
 };
+
 export default Tour;
