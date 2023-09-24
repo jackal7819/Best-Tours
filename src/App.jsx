4b0e86f24b3ee799771268849fb17adce0919782
Viktor Filippov <jackal3166@gmail.com>
2023-09-24 23:03:13 +0300

    Create Tours

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
 import Loading from './Loading';
+import Tours from './Tours';
 import { useEffect } from 'react';
 import { useState } from 'react';
 
 const URL = 'https://course-api.com/react-tours-project';
 
 const App = () => {
-    const [loading, setLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
     const [tours, setTours] = useState([]);
 
     const fetchTours = async () => {
-        setLoading(true);
+        setIsLoading(true);
         try {
             const response = await fetch(URL);
             const tours = await response.json();
-            console.log(tours);
+            setTours(tours);
         } catch (error) {
             console.log(error);
-        } finally {
-            setLoading(false);
         }
+        setIsLoading(false);
     };
 
     useEffect(() => {
         fetchTours();
     }, []);
 
-    if (loading) {
-        return <Loading />;
+    if (isLoading) {
+        return (
+            <main>
+                <Loading />
+            </main>
+        );
     }
 
-    return <h2>Tours Starter</h2>;
+    return (
+        <main>
+            <Tours tours={tours} />
+        </main>
+    );
 };
 
 export default App;
